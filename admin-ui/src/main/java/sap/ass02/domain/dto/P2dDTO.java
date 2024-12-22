3752f8883a827a74571be81547c454b99b7a8ed7
Marco Fontana <marco.fontana98@outlook.it>
2024-12-22 11:31:12 +0100

    docs(admin-ui): add documentation to P2dDTO and V2dDTO methods

--- admin-ui/src/main/java/sap/ass02/domain/dto/P2dDTO.java
+++ admin-ui/src/main/java/sap/ass02/domain/dto/P2dDTO.java
@@ -8,14 +8,28 @@ import sap.ddd.ValueObject;
  */
 public record P2dDTO(double x, double y) implements java.io.Serializable, ValueObject {
     
+    /**
+     * Sum of this point and a vector
+     * @param v vector to add
+     * @return new point
+     */
     public P2dDTO sum(V2dDTO v) {
         return new P2dDTO(this.x + v.x(), this.y + v.y());
     }
     
+    /**
+     * Difference of this point and another point
+     * @param v point to subtract
+     * @return new vector
+     */
     public V2dDTO sub(P2dDTO v) {
         return new V2dDTO(this.x - v.x(), this.y - v.y());
     }
     
+    /**
+     * String representation of this point
+     * @return string representation
+     */
     public String toString() {
         return "P2dDTO(" + this.x + "," + this.y + ")";
     }
--- admin-ui/src/main/java/sap/ass02/domain/dto/V2dDTO.java
+++ admin-ui/src/main/java/sap/ass02/domain/dto/V2dDTO.java
@@ -17,18 +17,36 @@ import sap.ddd.ValueObject;
  */
 public record V2dDTO(double x, double y) implements java.io.Serializable, ValueObject {
     
+    /**
+     * Method to get the x coordinate of a vector
+     * @return the x coordinate of this vector
+     */
     public double x() {
         return this.x;
     }
     
+    /**
+     * Method to get the y coordinate of a vector
+     * @return the y coordinate of this vector
+     */
     public double y() {
         return this.y;
     }
     
+    /**
+     * Method to sum two vectors
+     * @param v the vector to sum
+     * @return the sum of this vector with v
+     */
     public V2dDTO sum(V2dDTO v) {
         return new V2dDTO(this.x + v.x, this.y + v.y);
     }
     
+    /**
+     * Method to rotate two vectors of a certain degree
+     * @param degree the degree of rotation
+     * @return the rotation of this vector with v
+     */
     public V2dDTO rotate(double degree) {
         var rad = degree * Math.PI / 180;
         var cs = Math.cos(rad);
@@ -39,19 +57,36 @@ public record V2dDTO(double x, double y) implements java.io.Serializable, ValueO
         return v;
     }
     
+    /**
+     * Method to calculate the absolute value of a vector
+     * @return the absolute value of this vector
+     */
     public double abs() {
         return Math.sqrt(this.x * this.x + this.y * this.y);
     }
     
+    /**
+     * Method to calculate the normalized vector
+     * @return the normalized vector of this vector
+     */
     public V2dDTO getNormalized() {
         double module = Math.sqrt(this.x * this.x + this.y * this.y);
         return new V2dDTO(this.x / module, this.y / module);
     }
     
+    /**
+     * Method to calculate the scalar product of two vectors
+     * @param fact the vector to multiply
+     * @return the scalar product of this vector with v
+     */
     public V2dDTO mul(double fact) {
         return new V2dDTO(this.x * fact, this.y * fact);
     }
     
+    /**
+     * Method to return the string representation of a vector
+     * @return the string representation of this vector
+     */
     public String toString() {
         return "V2dDTO(" + this.x + "," + this.y + ")";
     }
