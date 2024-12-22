bf21cbefb4ff631744f9e6f28ed7529ee57408e3
Marco Fontana <marco.fontana98@outlook.it>
2024-12-22 12:16:49 +0100

    docs(admin-ui): add documentation for DDD objects

--- admin-ui/src/main/java/sap/ddd/Entity.java
+++ admin-ui/src/main/java/sap/ddd/Entity.java
@@ -2,6 +2,14 @@ package sap.ddd;
 
 import sap.ass02.domain.property.Jsonifyable;
 
+/**
+ * Interface for entities
+ * @param <T> the DTO type
+ */
 public interface Entity<T> extends Jsonifyable {
+    /**
+     * Get the DTO of the entity
+     * @return the DTO
+     */
     T toDTO();
 }
--- admin-ui/src/main/java/sap/ddd/ValueObject.java
+++ admin-ui/src/main/java/sap/ddd/ValueObject.java
@@ -1,4 +1,7 @@
 package sap.ddd;
 
+/**
+ * Marker interface for value objects
+ */
 public interface ValueObject {
 }
